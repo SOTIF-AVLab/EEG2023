0cc020289710c3e7d69c74e039e33b99c85d561c
Yingkai <yingkai.yang19@imperial.ac.uk>
2023-11-13 15:45:42 +0000

    fix bugs

--- Classifier/run_EEGModels.py
+++ Classifier/run_EEGModels.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import h5py
 import random
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report
 
 # 3000 datapoints downsampled to 750 for each trial
 # 63 channels are used for each trial
@@ -25,7 +27,7 @@ class Data():
                 data = h5py.File(
                     os.path.join(
                         self.data_path, 'sub' + str(self.sub_idx) + '_' + str(dataset[idx_set]) + '_data.mat'
-                    )
+                    ), 'r'
                 )
                 X1 = np.float32(np.transpose(data['X1']))
                 X2 = np.float32(np.transpose(data['X2']))
@@ -51,7 +53,7 @@ class Data():
                     
                     # concatenate X2 and X3
                     
-            All_X1 = np.concatenate((All_X1, All_X3), axis=2)
+            All_X2 = np.concatenate((All_X3, All_X2), axis=2)
             
             X1 = All_X1.copy()
             del All_X1
@@ -78,11 +80,13 @@ class Data():
         X1, X2 = self.read_data(idx)
         self.training_set = self.label_data(X1, X2)
         random.shuffle(self.training_set)
+        return self.training_set
         
     def prepare_testing_data(self):
         idx = np.array([2, 3, 4])
         X1, X2 = self.read_data(idx)
         self.testing_set = self.label_data(X1, X2)
+        return self.testing_set
     
     def get_data(self, dataset):        
         X1, X2 = self.read_data(dataset)
@@ -93,9 +97,9 @@ subject_number = [1,2,3,4,5,6,7,8,10,11,12]
 current_dir = os.getcwd()
 
 # define model1 as EEGNet, SharllowConvNet and DeepConvNet
-model1 = EEGNet(nb_classes = 2, Chans = 63, Samples = 750)
-model2 = ShallowConvNet(nb_classes = 2, Chans = 63, Samples = 750)
-model3 = DeepConvNet(nb_classes = 2, Chans = 63, Samples = 750)
+model1 = EEGNet(nb_classes = 2, Chans = 60, Samples = 750)
+model2 = ShallowConvNet(nb_classes = 2, Chans = 60, Samples = 750)
+model3 = DeepConvNet(nb_classes = 2, Chans = 60, Samples = 750)
 
 
 for subject in subject_number:
@@ -103,10 +107,9 @@ for subject in subject_number:
     for event_Number in eventNumber:
         event_path = f'{subject_path}\event{event_Number}'
         data = Data(event_path, subject, event_Number)
+
         training_set = data.prepare_training_data()
         testing_set = data.prepare_testing_data()
-
-        print(training_set)
 
         train_data = [item[0] for item in training_set]
         x_train = np.stack(train_data, axis=2)
@@ -118,9 +121,24 @@ for subject in subject_number:
         test_label = [item[1] for item in testing_set]
         y_test = np.stack(test_label)
 
+        # permute the data
+        x_train = np.transpose(x_train, (2, 0, 1))
+        x_test = np.transpose(x_test, (2, 0, 1))
+        y_train = to_categorical(y_train)
+
+        # reshape the data  
+
+        print(np.shape(x_train))
+        print(np.shape(y_train))
+        print(np.shape(x_test))
+
 
         # change model to model1, model2, and model3 to try on different sota models 
         model1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        fittedModel = model1.fit(x_train, y_train, batch_size=16, epochs=300, verbose=2, validation_split=0.2)
-        predicted = model1.predict(x_test, y_test)
-        print(predicted)
+        fittedModel = model1.fit(x_train, y_train, epochs=100, validation_split=0.2)
+        predicted = model1.predict(x_test)
+        predicted_labels = np.argmax(predicted, axis=1)
+        # Generate a classification report
+        report = classification_report(y_test, predicted_labels)
+
+        print(report)
